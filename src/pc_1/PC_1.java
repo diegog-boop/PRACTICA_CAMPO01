9fce167ef23198b1f06dc3e136670bc5cdbc6816
HP <HP@192.168.1.41>
2026-04-30 12:12:32 -0500

    first commit

---
+++ src/pc_1/PC_1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pc_1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author FAMILIA
+ */
+public class PC_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ArrayList <String> frutas = new ArrayList<>();
+        frutas.add("Manzana");
+        frutas.add("Platano");
+        frutas.add("Pera");
+        
+        System.out.println("Fruta con posicion 1: " + frutas.get(1));
+        
+      
+        
+        
+    }
+    
+}
